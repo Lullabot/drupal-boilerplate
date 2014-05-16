73257e1779fc648e44afe9637afdfa8e9f2e0402
Dave Reid <dave@davereid.net>
2014-05-16 09:42:01 -0500

    Adding Drush port of https://github.com/Lullabot/casperjs_foundation.

---
+++ drush/commands/casperjs/common.js
@@ -0,0 +1,26 @@
+/**
+ * Helper methods for navigating through a site.
+ *
+ * This file is included automagically by the "test" executable.
+ */
+
+/**
+ * Wrapper for http://docs.casperjs.org/en/latest/modules/casper.html#open
+ *
+ * Uses url argument from the command line in order to open a URL path.
+ */
+casper.openPath = function (path) {
+  var cleanPath = path.replace(/^\//, '');
+  return casper.open(casper.cli.get('url') + '/' + cleanPath);
+};
+
+/**
+ * Wrapper for http://docs.casperjs.org/en/latest/modules/casper.html#thenopen
+ *
+ * Uses url argument from the command line in order to open a URL path and
+ * define a navigation step.
+ */
+casper.thenOpenPath = function (path, thenCallback) {
+  var cleanPath = path.replace(/^\//, '');
+  return casper.thenOpen(casper.cli.get('url') + '/' + cleanPath, thenCallback);
+};
